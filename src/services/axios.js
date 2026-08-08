fc9436d84997cd3b40fdb6354629f4a466b53b14
developer786-cloud <developerbablons786@gmail.com>
2026-08-08 16:21:28 +0530

    backend news artical eroro solve

--- src/services/axios.js
+++ src/services/axios.js
@@ -6,7 +6,7 @@ const api = axios.create({
 })
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('bablons_admin_token')
+  const token = localStorage.getItem('bablons_admin_token') || localStorage.getItem('adminToken')
   if (token) config.headers.Authorization = `Bearer ${token}`
   return config
 })
